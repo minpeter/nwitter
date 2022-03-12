270082ce59b6531d799eef76688fe49ab4ce4f36
minpeter <kali2005611@gmail.com>
2022-03-12 23:34:53 +0900

    finishing set up

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { Router } from "react-router-dom";
+import AppRouter from "./Router";
+
+function App() {
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
