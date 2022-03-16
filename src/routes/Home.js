e437a0cf3dcd0496b2f06ee6545d099846728cf7
minpeter <kali2005611@gmail.com>
2022-03-16 12:22:49 +0900

    #3.0 Form and Database Setup done.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,30 @@
 import React from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = React.useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const onChage = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  <div>
+    <form>
+      <input
+        value={nweet}
+        onChange={onChage}
+        type="text"
+        placeholder="Waht's on your mind?"
+        maxLength={120}
+      />
+      <input type="submit" value="Nweet" />
+    </form>
+  </div>;
+};
+
 export default Home;
